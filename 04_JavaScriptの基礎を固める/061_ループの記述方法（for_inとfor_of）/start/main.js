be44eefc725b6fcebc8e0cfa2965a1c3e9522041
Noi Fujii <noinoi51267@gmail.com>
2022-03-13 20:11:32 +0900

    [Add]JavaScriptのループ(for in/for of)

--- 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
+++ 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
@@ -1,5 +1,9 @@
 const arry = [1, 2, 3, 4, 5, 6];
 
-for (let i = 0; i < arry.length; i++) {
-    console.log(i);
+for (let i in arry) {
+    let v = arry[i];
+    console.log(v);
 }
+
+//i,j,kは添字という意味合いが強い。
+// for inでは添字が、for ofでは値が渡ってくる(対応していないのもある)
